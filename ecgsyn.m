48ee956428a86d3f63076de6b116071086c4591f
Manav Kataria <Manav Kataria@localhost>
2010-05-10 19:30:40 +0530

    Version 0.3: Baseline drift; Version 0.2: X,Y,Z Phase Sim

--- ecgsyn.m
+++ ecgsyn.m
@@ -1,4 +1,7 @@
 % ECGSYN
+
+% Version 0.3: Baseline drift that gave the R wave.
+% Version 0.2: X,Y,Z Phase Sim
 % Version 0.1: Crude Simulation
 % X,Y Phase and State Trajectories plot; No Z.
 
@@ -6,17 +9,17 @@ clear all;
 clc;
 
 %%
-t0 = 0; tf = 12;
-x0 = -0.2; y0 = 2;
-options = odeset('OutputFcn',@odephas2);
+t0 = 0; tf = 20;
+x0 = -0.2; y0 = 2; z0 = 0;
+options = odeset('OutputFcn',@odephas3);
 
 figure;
-[T,Y] = ode45(@ecgsynSolver, [t0 tf], [x0 y0], options);
+[T,Y] = ode45(@ecgsynSolver, [t0 tf], [x0 y0 z0], options);
 
 figure;
-plot(T,Y(:,1),'-',T,Y(:,2),'-'); %,T,Y(:,3),'.')
+plot(T,Y(:,1),'-', T,Y(:,2),'-', T,Y(:,3),'.-');
 title('State Trajectories as a function of Time');
-legend('state x','state y'); grid on;
-xlabel('Time \rightarrow '); ylabel('x(t) & y(t) \rightarrow ');
-% text(-pi/4,sin(-pi/4),'\leftarrow sin(-\pi\div4)','HorizontalAlignment','left')
-
+legend('state x','state y', 'state z'); grid on;
+xlabel('Time \rightarrow '); ylabel('x(t), y(t) & z(t) \rightarrow ');
+% text(-pi/4,sin(-pi/4),'\leftarrow
+% sin(-\pi\div4)','HorizontalAlignment','left')
--- ecgsynSolver.m
+++ ecgsynSolver.m
@@ -1,9 +1,22 @@
 %% Ecgsyn Solver
-%  Version 0.1 2d Version
+%  Version 0.3 baseline drift;
+%  Version 0.2 3d Version, 9th March 
+%  Version 0.1 2d Version, 8th March
 function dy = ecgsynSolver(t,y)
 
-radius = 1 - sqrt(y(1)*y(1) + y(2)*y(2));
-w = 1; dy = zeros(2,1);     % a column vector
+radius = 1 - sqrt(y(1)*y(1) + y(2)*y(2)); %limit cycle
+w = 1;       dy = zeros(3,1);       
+a = 30.0;    b  = 0.1;
+A = 0.15;    f2 = 0.01;  
 
-dy(1) = radius*y(1) -      w*y(2);
-dy(2) =      w*y(1) + radius*y(2);
+dy(1) = radius*y(1) -      w*y(2); 
+dy(2) =      w*y(1) + radius*y(2); 
+dy(3) =  a*mod( atan2(y(2),y(1)), 2*pi)* ... 
+      exp(-mod( atan2(y(2),y(1)), 2*pi)* ... 
+           mod( atan2(y(2),y(1)), 2*pi)/ ... 
+           (2*b*b)) - (y(3) - A*sin(2*pi*f2*t));
+
+% dy(3) =  a*mod( atan2(y(2),y(1)), 2*pi)* ... 
+%       exp(-mod( atan2(y(2),y(1)), 2*pi)* ... 
+%            mod( atan2(y(2),y(1)), 2*pi)/ ... 
+%            (2*b*b)) - (y(3) - A*sin(2*pi*f2*t));
