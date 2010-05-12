e132ad5b2d494fd5ab8c1c646266c228a7cc8f45
Manav Kataria <Manav Kataria@localhost>
2010-05-12 15:23:22 +0530

    V0.81: VCG and Validation thru Linear Matrix transformation

--- ecgsyn.m
+++ ecgsyn.m
@@ -1,8 +1,9 @@
 % ECGSYN
 
-% Version 0.7: ECG Validation: Attemtps 
-%	       using datafile: ecg_normal.gif
-% Version 0.6: 3 Vector CardioGram: Polar & Time Series
+% Version 0.81: VCG and Validation thru Linear Matrix transformation
+% Version 0.7 (deprecated): ECG Validation: Attemtps 
+%              using datafile: ecg_normal.gif
+% Version 0.6 (deprecated): 3 Vector CardioGram: Polar & Time Series
 %              Also explored; lead2 derived from lead1.
 % Version 0.5: 3 Lead ECG (realistic); Polar Plot; 
 % Version 0.4: 2 Lead ECG
@@ -43,51 +44,50 @@ legend('Lead 2', 'Lead 1','Location','SouthWest');
 xlabel('Time \rightarrow '); ylabel('milivolts (mv) \rightarrow ');
 grid on; grid minor; title('Lead 2'); ylim([-0.5 0.5]);
 
-%% calculate Lead 3
+%% calculate Cardiac Dipole
+% Linear Transformation Matrix - M = [1 0; -1/sqroot(3) 2/sqrt(3)]
 
-T3 = T1;
-% To determine whether angle subtended by dipole wrt horizontal is positive
-% or negative; Cos(60-angle) is the lead2 value at horizontal 
-% zeroComponentonY2 = cos (pi/3); 
-% for i=1:length(T3) 
-%     if (Y2(i,3) > zeroComponentonY2*Y1(i,3))
-%         Y3(i) = cos (2*pi/3 + acos(Y1(i,3)));
-%     else
-%         Y3(i) = cos (2*pi/3 - acos(Y1(i,3)));
-%     end
-% end
-% plot(T3, Y3, T2, Y2(:,3), T1, Y1(:,3));
-
-%%
-
+display 'Derive Dipole';
 display 'Derive Lead 3';
+
+r3 = sqrt(3);       %root3
+M = [1 0; -1/r3 2/r3];
+D = zeros(datapts,4);
+Y3 = zeros(datapts,1);
+
+for i=1:datapts 
+     D(i,1:2) = M*[Y1(i);Y2(i)];        % (x,y)
+     D(i,3) = D(i,1)^2+D(i,2)^2;        % Magnitude
+     D(i,4) = atan2(D(i,2),D(i,1))*180/pi; % Angle
+     Y3(:,i) = -D(i,3)/2*cos(D(i,4)-r3/2*sin(D(i,4)));
+end
+%%
 subplot(3,1,3); 
-% from Lead 2:
-plot(T2, cos (pi/3 - acos(Y2(:,3)))-0.866, T2, Y2(:,3), T1, Y1(:,3));
-
-% from Lead 1:
-% plot(T1, cos (2*pi/3 - acos(Y1(:,3)))-0.866, T2, Y2(:,3), T1, Y1(:,3));
-
-% figure;
-% Lead 3 theta should be derived from either Y1 or Y2 or a mix of both?
-% plot(T1,cos (2*pi/3 - acos(Y1(:,3))), T1, cos (pi/3 - acos(Y2(:,3))), 
-% T1,(cos (2*pi/3 - acos(Y1(:,3)))+cos (pi/3 - acos(Y2(:,3))))/2);
+plot(T1, Y3(:,1), T2, Y2(:,3), T1, Y1(:,3));
 legend('Lead 3 derived','Lead 2','Lead 1','Location','SouthWest');
 xlabel('Time \rightarrow '); ylabel('milivolts (mv) \rightarrow ');
 grid on; grid minor; title('Lead 3'); ylim([-0.5 0.5]);
+title('Lead3');
 
+%%
+figure;
+plot(T1, Y3(:,1), T2, Y2(:,3), T1, Y1(:,3));
+legend('Lead 3 derived','Lead 2','Lead 1','Location','SouthWest');
+xlabel('Time \rightarrow '); ylabel('milivolts (mv) \rightarrow ');
+grid on; grid minor; title('Lead 3'); ylim([-0.5 0.5]);
+title('Lead3');
 
 %% Vector CardioGram
+
 figure; subplot(2,1,1);
 display 'Vector CardioGram: Polar';
-polar(pi/3 - acos(Y2(:,3)),Y2(:,3),'--r');
-
-% polar(acos(Y1(:,3)),Y1(:,3),'--r');
+polar(D(:,4),D(:,3));
 grid on; title('Vector CardioGram: Polar'); 
 
 subplot(2,1,2);
-plot(T2, pi/3 - acos(Y2(:,3)),'.-');
-%plot(T1, acos(Y1(:,3)),'.-');
+plot(T1,D(:,3),T1,D(:,4));
+legend('Magnitude','Angle');
+title('Dipole');
 display 'Vector CardioGram: Time Series';
 grid on; title('Vector CardioGram: Time Series'); 
 xlabel('Time \rightarrow '); ylabel('angle (radian) \rightarrow ');
