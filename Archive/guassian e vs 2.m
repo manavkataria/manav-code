db65a781d17424fdc4bfe66195859582e4f1919f
Manav Kataria <Manav Kataria@localhost>
2011-06-05 03:40:09 +0530

    Temp Commit Before System Upgrade

--- Archive/guassian e vs 2.m
+++ Archive/guassian e vs 2.m
@@ -1,8 +1,11 @@
 %test plot of e^-(th/b)^2
 t=-10:0.1:10;
-f=exp(-t.*t);
-f2=2.^(-t.*t);
-plot(t,f,t,f2);
-legend ('e^-(t^2)' ,'2^-(t^2)');
+f=exp(-t);
+%f=exp(-t.*t);
+% f2=2.^(-t.*t);
+% plot(t,f,t,f2);
+plot(t,f);
+% legend ('e^-(t^2)' ,'2^-(t^2)');
 
-plot (t, exp(-(t-.3).^2), t, exp(-t.^2), t, exp(t.^2), t, exp(t))
+%plot (t, exp(-(t-.3).^2), t, exp(-t.^2), t, exp(t.^2), t, exp(t))
+
